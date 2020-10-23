331c0329299e5bbf5ce064aac6f116bb5f297706
link-007 <67220384+link-007@users.noreply.github.com>
2020-10-23 21:20:24 +0800

    Add files via upload

---
+++ logs/logs.vue
@@ -0,0 +1,30 @@
+<template>
+	<!-- <view class="container log-list">
+	  <block v:for="{{logs}}" wx:for-item="log">
+	    <text class="log-item">{{index + 1}}. {{log}}</text>
+	  </block>
+	</view> -->
+</template>
+
+<script>
+	
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.log-list {
+	  display: flex;
+	  flex-direction: column;
+	  padding: 40rpx;
+	}
+	.log-item {
+	  margin: 10rpx;
+	}
+
+</style>
